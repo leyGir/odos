d2d4d3afc107d3c0738a015cb6008758d52ce7f3
Soraya <soraya.meystre@heig-vd.ch>
2020-09-28 11:52:26 +0200

    Connection informations

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/odos');
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/odos', { useNewUrlParser: true, useUnifiedTopology: true });
 
 var app = express();
 
