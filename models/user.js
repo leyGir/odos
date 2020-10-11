89b2c354733d39d944745298adf42f409862bc3f
Yasmine Hamdan <yasmine.hamdan@heig-vd.ch>
2020-10-11 23:38:19 +0200

    route et model user

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const uniqueValidator = require('mongoose-unique-validator');
+
+// Define the schema for users
+const userSchema = new Schema({
+  username: {
+      type : String,
+      match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+      required: [true, "can't be blank"],
+      unique: true,
+      validate: {
+        validator: UsernameUnique,
+        message: 'List name {VALUE} already exists'
+    }
+  },
+  email:{
+      type : String,
+      match: [/\S+@\S+\.\S+/, 'is invalid'],
+      required: [true, "can't be blank"],
+      unique: true,
+      validate: {
+        validator: EmailUnique,
+        message: 'List name {VALUE} already exists'
+    }
+  },
+  //A faire! Hash password
+  password:{
+      type : String,
+
+  },
+  registrationDate:{
+    type: Date,
+    default: Date.now
+  },
+});
+
+// Create the model from the schema and export it
+module.exports = mongoose.model('User', userSchema);
+
+UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,57 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/user');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.find().sort('username').exec(function(err, users) {
+    if (err) {
+      return next(err);
+    }
+    res.send(users);
+  });
 });
 
+module.exports = router;
+
+
+/* POST new user */
+router.post('/', function(req, res, next) {
+  // Create a new document from the JSON in the request body
+  const newUser = new User(req.body);
+  // Save that document
+  newUser.save(function(err, savedUser) {
+    if (err) {
+      return next(err);
+    }
+    // Send the saved document in the response
+    res.send(savedUser);
+  });
+});
+
+
+///////////////////
+
+/* PUT/modify username */
+router.put('/', function(req, res, next) {
+  User.find().sort('name').exec(function(err, users) {
+    if (err) {
+      return next(err);
+    }
+    res.send(users);
+  });
+});
+module.exports = router;
+
+
+/* DELETE user */
+router.delete('/', function(req, res, next) {
+  User.find().sort('name').exec(function(err, users) {
+    if (err) {
+      return next(err);
+    }
+    res.send(users);
+  });
+});
 module.exports = router;
